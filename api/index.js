94b48f58039f5cf2474d1195b8686def5540edc3
Jiahui Chen <jchen23@stanford.edu>
2022-03-13 15:36:00 -0700

    added dataclass and api configuration

--- api/index.js
+++ api/index.js
@@ -1,19 +1,30 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
+import { MongoClient } from "mongodb";
+
+let DATABASE_NAME = "final_project";
 
 const api = express.Router();
+let conn = null;
+let db = null;
+let Users = null;
+let Posts = null;
 
 const initApi = async app => {
   app.set("json spaces", 2);
   app.use("/api", api);
+  conn = await MongoClient.connect("mongodb://localhost");
+  db = conn.db(DATABASE_NAME);
+  Users = db.collection("user");
+  Posts = db.collection("post");
 };
 
 api.use(bodyParser.json());
 api.use(cors());
 
 api.get("/", (req, res) => {
-  res.json({ message: "Hello, world!" });
+  res.json({ db: DATABASE_NAME });
 });
 
 export default initApi;
